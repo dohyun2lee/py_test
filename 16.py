d86dbf1ed9f8b0efeb75bd93e6f61f4a4f9caad8
dohyun2lee <eric8800@naver.com>
2023-08-28 10:54:32 +0900

    .

---
+++ 16.py
@@ -0,0 +1,10 @@
+s = "banana"
+
+if 'a' in s :
+    if 'b' in 'banana' :
+        print("banana에는 a와 b가 있다.")
+    
+if 'a' in s or 'c' in s :
+    print("banana에는 a나 c가 있다.")
+    
+print('a' in s)
